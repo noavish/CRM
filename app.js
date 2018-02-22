98e9e7e73b45cb94ab68fb32f5a8988483f56b16
noavish <noavish1@gmail.com>
2018-02-22 10:36:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var customer = require('./routes/customer');
+var company = require('./routes/company');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use('/api/customer', customer);
+app.use('/api/company', company);
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : '< MySQL username >',
+  password : '< MySQL password >',
+  database : '<your database name>'
+});
+
+connection.connect();
+connection.end();
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
